6abb2f15cf8dc0795aecdad154c24ffd276dfb09
Peter Lyubenov <peter.lyubenov98@gmail.com>
2021-01-15 19:03:34 +0200

    clean up shop page code

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,14 +7,12 @@ class ShopPage extends Component {
     constructor(props) {
         super(props);
         this.state = { collections }
-
-        console.log(props.match);
     }
     render() { 
         const { collections } = this.state;
         return ( <div className="shop-page">
             {collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
+                <CollectionPreview key={id} {...otherCollectionProps} />
             ))}
         </div> );
     }
